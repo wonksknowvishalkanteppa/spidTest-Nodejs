691b4209942617703372b99c9f1d28e477340cba
wonksknowvishalkanteppa <vishal.mahesh@youngwonks.com>
2021-04-05 13:21:08 +0530

    speed

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ options = {
 (async() => {
     try {
         var x = await speedTest(options);
-        console.log(x['upload']);
+        console.log(x['upload']['bandwidth'] * 8 / 1024 / 1024);
+        console.log("\n\n", x);
     } catch (err) {
         console.log(err.message);
     } finally {
